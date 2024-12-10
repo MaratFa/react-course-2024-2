2b9eee3d80622887d9c6b559e0650e3f93f5eb2d
MaratFa <evolution99@rambler.ru>
2024-12-11 02:42:56 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Food from "./Food";
 
 function App() {
   return (
     <>
+      <Food />
       <Header />
       <Footer />
     </>
