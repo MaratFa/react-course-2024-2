aff19cb55ab20ce635f7d5d362dc83fad6aef974
MaratFa <evolution99@rambler.ru>
2024-12-16 03:27:59 +0300

    edited

--- 4-props/src/App.jsx
+++ 4-props/src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "./Button";
+import Student from "./Student.jsx";
 
 function App() {
   return (
     <>
-      <Button />
+      <Student name="Leo" age={40} isStudent={true} />
+      <Student name="Ken" age={30} isStudent={false} />
+      <Student name="Bob" age={20} isStudent={false} />
+      <Student name="Den" age={24} isStudent={true} />
+      <Student />
     </>
   );
 }
