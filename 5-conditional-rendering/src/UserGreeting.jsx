007a7dafa5964192d39dff1eb368aaedef495803
MaratFa <evolution99@rambler.ru>
2024-12-18 07:02:18 +0300

    Edited

--- 5-conditional-rendering/src/UserGreeting.jsx
+++ 5-conditional-rendering/src/UserGreeting.jsx
@@ -1,5 +1,15 @@
-const UserGreeting = (props) => {
-  return <div>UserGreeting</div>;
-};
+import PropTypes from "prop-types";
 
+function UserGreeting(props) {
+  return props.isLoggedIn ? (
+    <h2 className="welcome-message">Welcome {props.username}</h2>
+  ) : (
+    <h2>Please log in to continue</h2>
+  );
+}
+
+UserGreeting.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  username: PropTypes.string,
+};
 export default UserGreeting;
