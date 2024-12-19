b4a02591215a7130907692e5bef43a1652d5951e
MaratFa <evolution99@rambler.ru>
2024-12-19 23:23:22 +0300

    Edited

--- 5-conditional-rendering/src/App.jsx
+++ 5-conditional-rendering/src/App.jsx
@@ -3,7 +3,9 @@ import UserGreeting from "./UserGreeting";
 function App() {
   return (
     <>
+      <UserGreeting isLoggedIn={false} username="Bob" />
       <UserGreeting isLoggedIn={true} username="Bob" />
+      <UserGreeting />
     </>
   );
 }
