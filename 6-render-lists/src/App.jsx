862e76435045874483e2e03a63edaf645c8ac9f0
MaratFa <evolution99@rambler.ru>
2024-12-21 20:42:54 +0300

    edited

--- 6-render-lists/src/App.jsx
+++ 6-render-lists/src/App.jsx
@@ -2,11 +2,7 @@ import UserGreeting from "./UserGreeting";
 
 function App() {
   return (
-    <>
-      <UserGreeting isLoggedIn={false} username="Bob" />
-      <UserGreeting isLoggedIn={true} username="Bob" />
-      <UserGreeting />
-    </>
+   
   );
 }
 
