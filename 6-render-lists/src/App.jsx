557ff4e37dc8255f8bd76de30e607ea6b56380ed
MaratFa <evolution99@rambler.ru>
2024-12-23 02:17:23 +0300

    Edited

--- 6-render-lists/src/App.jsx
+++ 6-render-lists/src/App.jsx
@@ -9,7 +9,20 @@ function App() {
     { id: 5, name: "pineapple", calories: 110 },
   ];
 
-  return <List items={fruits} category='Fruits'/>;
+  const vegetables = [
+    { id: 6, name: "potatoes", calories: 110 },
+    { id: 7, name: "celery", calories: 15 },
+    { id: 8, name: "carrots", calories: 25 },
+    { id: 9, name: "corn", calories: 63 },
+    { id: 10, name: "broccoli", calories: 50 },
+  ];
+
+  return (
+    <>
+      <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
+    </>
+  );
 }
 
 export default App;
