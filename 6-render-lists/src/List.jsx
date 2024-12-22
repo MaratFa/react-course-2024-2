1d523749c6187df0a9df71dc879bd3201720bff8
MaratFa <evolution99@rambler.ru>
2024-12-22 15:23:42 +0300

    Edited

--- 6-render-lists/src/List.jsx
+++ 6-render-lists/src/List.jsx
@@ -1,11 +1,25 @@
-const List = () => {
-  const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+function List() {
+  const fruits = [
+    { id: 1, name: "apple", colories: 90 },
+    { id: 2, name: "orange", colories: 40 },
+    { id: 3, name: "banana", colories: 100 },
+    { id: 4, name: "coconut", colories: 120 },
+    { id: 5, name: "pineapple", colories: 110 },
+  ];
 
-  const listItems = fruits.map(fruit => <li>{fruit}</li>)
+  // fruits.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABETICAL
+  // fruits.sort((a, b) => b.name.localeCompare(a.name)); // REVERSE ALPHABETICAL
+  // fruits.sort((a, b) => a.colories - b.colories);      // ALPHABETICAL NUMERIC
+  // fruits.sort((a, b) => b.colories - a.colories);      // REVERSE NUMERIC
 
-  return (<>
-  {listItems} {console.log(listItems)}
-  </>);
-};
+  const listItems = fruits.map((fruit) => (
+    <li key={fruit.id}>
+      {fruit.name}:&nbsp;
+      <b>{fruit.colories}</b> colories
+    </li>
+  ));
+
+  return <ol>{listItems}</ol>;
+}
 
 export default List;
