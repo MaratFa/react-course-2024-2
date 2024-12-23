4754f08e9d05df9a920b948abd0e1d14857076fe
MaratFa <evolution99@rambler.ru>
2024-12-23 03:06:47 +0300

    Edited

--- 6-render-lists/src/List.jsx
+++ 6-render-lists/src/List.jsx
@@ -1,20 +1,6 @@
 import PropTypes from "prop-types";
 
-List.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      calories: PropTypes.number,
-    })
-  ),
-  category: PropTypes.string,
-};
-
-function List(props) {
-  const { category } = props;
-  const { items } = props;
-
+function List({ category = "Category", items = [] }) {
   const listItems = items.map((item) => (
     <li key={item.id}>
       {item.name}:&nbsp;<b>{item.calories}</b> colories
@@ -28,5 +14,16 @@ function List(props) {
     </>
   );
 }
+
+List.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      calories: PropTypes.number,
+    })
+  ),
+  category: PropTypes.string,
+};
 
 export default List;
