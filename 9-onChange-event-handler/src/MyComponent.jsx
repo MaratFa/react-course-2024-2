1c8bd745b50097ec9cfbfe807cbb0ba4cfa0d077
MaratFa <evolution99@rambler.ru>
2025-01-02 03:58:40 +0300

    Edited

--- 9-onChange-event-handler/src/MyComponent.jsx
+++ 9-onChange-event-handler/src/MyComponent.jsx
@@ -1,30 +1,16 @@
 import { useState } from "react";
 
 const MyComponent = () => {
-  const [name, setName] = useState("Guest");
-  const [age, setAge] = useState(0);
-  const [isEmployed, setIsEmployed] = useState(false);
+  const [name, setName] = useState("");
 
-  const updateName = () => {
-    setName("Spongebob");
-  };
-
-  const incrementAge = () => {
-    setAge(age + 1);
-  };
-
-  const toggleEmployedStatus = () => {
-    setIsEmployed(!isEmployed);
-  };
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
 
   return (
     <div>
+      <input value={name} onChange={handleNameChange} />
       <p>Name: {name}</p>
-      <button onClick={updateName}>Set Name</button>
-      <p>Age: {age}</p>
-      <button onClick={incrementAge}>Increment Age</button>
-      <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
-      <button onClick={toggleEmployedStatus}>Toggle Status</button>
     </div>
   );
 };
