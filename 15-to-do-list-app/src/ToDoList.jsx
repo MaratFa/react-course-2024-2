eb124b1a69478849539a00885255953fb45c24bb
MaratFa <evolution99@rambler.ru>
2025-01-10 07:21:12 +0300

    Edited

--- 15-to-do-list-app/src/ToDoList.jsx
+++ 15-to-do-list-app/src/ToDoList.jsx
@@ -1,50 +1,57 @@
 import { useState } from "react";
 
 const ToDoList = () => {
+  const [tasks, setTasks] = useState([
+    "Eat breakfast",
+    "Take a shower",
+    "Walk the dog",
+  ]);
+  const [newTask, setNewTask] = useState("");
 
-  const [tasks, setTasks] = useState(['Eat Breakfast', 'Take a shower', 'Walk the dog']);
-  const [newTask, setNewTask] = useState('');
-
-  function handleInputChange(event){
+  function handleInputChange(event) {
     setNewTask(event.target.value);
   }
 
   function addTask() {
 
+    if(newTask.trim() !== ''){
+      setT
+    }
+    setTasks([...tasks, newTask]);
   }
 
-  function deleteTask(index) {
+  function deleteTask(index) {}
 
-  }
+  function moveTaskUp(index) {}
 
-  function moveTaskUp(index) {
-
-  }
-
-  function moveTaskDown(index) {
-
-  }
+  function moveTaskDown(index) {}
 
   return (
     <div className="to-do-list">
-      
       <h1>To-Do-List</h1>
 
-      <input 
+      <input
         type="text"
         placeholder="Enter a task..."
-        value={newTask}  
+        value={newTask}
         onChange={handleInputChange}
       />
-      <button
-        className="add-button"
-        onClick={addTask}
-      >
+      <button className="add-button" onClick={addTask}>
         Add
       </button>
 
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+              <span className="text">{task}</span>
+              <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+              <button className="move-button" onClick={() => moveTaskUp(index)}>Up</button>
+              <button className="move-button" onClick={() => moveTaskDown(index)}>Down</button>
+          </li> 
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
