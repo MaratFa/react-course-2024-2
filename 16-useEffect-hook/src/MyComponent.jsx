67a8333398313a601aabdbd7f9537caa03514d32
MaratFa <evolution99@rambler.ru>
2025-01-15 06:49:08 +0300

    Edited

--- 16-useEffect-hook/src/MyComponent.jsx
+++ 16-useEffect-hook/src/MyComponent.jsx
@@ -1,17 +1,33 @@
 import { useState, useEffect } from "react";
 
 const MyComponent = () => {
-  
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('green');
 
-  function addCount(){
-    setCount(c => c + 1);
+  useEffect(
+    function () {
+      document.title = `Count: ${count} ${color}`;
+    }, []
+  );
+
+  function addCount() {
+    setCount((c) => c + 1);
+  }
+  
+  function subtractCount() {
+    setCount((c) => c - 1);
+  }
+
+  function changeColor() {
+    setColor(c => c === 'red' ? 'green' : 'red')
   }
 
   return (
     <>
-    <p>Count: {count}</p>
-    <button onClick={addCount}>Add</button>
+      <p style={{color: color}}>Count: {count} <br/> Color: {color}</p>
+      <button onClick={addCount}>Add</button>
+      <button onClick={subtractCount}>Subtract</button><br />
+      <button onClick={changeColor}>Change Color</button>
     </>
   );
 };
